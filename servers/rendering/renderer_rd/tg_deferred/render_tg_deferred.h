fda0b2434769caa1fbb0b40d09c1d2ef1a2d55a4
baolin.yin <121343201+treasureGrove@users.noreply.github.com>
2026-07-15 17:59:10 +0800

    Update render_tg_deferred.h

---
+++ servers/rendering/renderer_rd/tg_deferred/render_tg_deferred.h
@@ -0,0 +1,17 @@
+/**************************************************************************/
+/*  render_tg_deferred.h                                                  */
+/**************************************************************************/
+
+#pragma once
+
+#include "servers/rendering/renderer_rd/renderer_scene_render_rd.h"
+
+namespace RendererSceneRenderImplementation{
+
+class RenderTGDeferred : public RendererSceneRenderRD {
+    GDCLASS(RenderTGDeferred, RendererSceneRenderRD);
+protected:
+    
+};
+
+}// namespace RendererSceneRenderImplementation
